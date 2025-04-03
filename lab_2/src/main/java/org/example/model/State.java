c0e4ca7f60fc947c41be7e4b363ee4a3746668a0
Max <steblevskiymaxim@ua.fm>
2025-04-04 01:13:57 +0300

    Delete lab_2 directory

--- lab_2/src/main/java/org/example/model/State.java
+++
@@ -1,6 +0,0 @@
-package org.example.model;
-
-public enum State {
-    MALE,
-    FEMALE
-}
--- lab_2/src/main/java/org/example/model/Student.java
+++
@@ -1,7 +0,0 @@
-package org.example.model;
-
-public class Student extends Human {
-    public Student(String firstName, String lastName, String middleName, State state) {
-        super(firstName, lastName, middleName, state);
-    }
-}
